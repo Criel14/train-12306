f70f1cb535b551f8421be23122efd6e8eb7d8ef1
Criel14 <1298373079@qq.com>
2025-03-09 17:27:17 +0800

    member：测试注册功能（未完善）

--- member/src/main/java/com/criel/train/member/controller/MemberController.java
+++ member/src/main/java/com/criel/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.criel.train.member.controller;
 import com.criel.train.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,13 @@ public class MemberController {
     private MemberService memberService;
 
     @GetMapping("/testConnect")
-    public Long testConnect() {
+    public long testConnect() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long  register(String mobile) {
+        return memberService.register(mobile);
     }
 
 }
--- member/src/main/java/com/criel/train/member/service/MemberService.java
+++ member/src/main/java/com/criel/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.criel.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.criel.train.member.domain.Member;
+import com.criel.train.member.domain.MemberExample;
 import com.criel.train.member.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,6 +17,29 @@ public class MemberService {
 
     public long count() {
         return memberMapper.countByExample(null);
+    }
+
+    /**
+     * 会员注册功能，暂未完善，供测试
+     * @param mobile 手机号
+     * @return 用户id
+     */
+    public long register(String mobile) {
+        // 重复检测
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> memberList = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(memberList)) {
+            throw new RuntimeException("手机号已被注册");
+        }
+
+        // 创建新member
+        Member member = new Member();
+        // 测试用：暂时用系统时间做id
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 
 }
