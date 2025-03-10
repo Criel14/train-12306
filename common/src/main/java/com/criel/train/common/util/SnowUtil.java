00adb9424d29aeecf5e397073e458c24c13ea2c4
Criel14 <1298373079@qq.com>
2025-03-10 17:20:07 +0800

    common：新增工具类，使用雪花算法生成UID

---
+++ common/src/main/java/com/criel/train/common/util/SnowUtil.java
@@ -0,0 +1,26 @@
+package com.criel.train.common.util;
+
+import cn.hutool.core.util.IdUtil;
+
+/**
+ * 封装hutool雪花算法
+ */
+public class SnowUtil {
+
+    /**
+     * TODO 以下2个值每台机器都不一样，后面可能需要配置到数据库，启动时从数据库读取
+     */
+    // 数据中心
+    private static long dataCenterId = 1;
+    // 机器标识
+    private static long workerId = 1;
+
+    // 用Hutool工具的雪花算法生成id
+    public static long getSnowflakeNextId() {
+        return IdUtil.getSnowflake(workerId, dataCenterId).nextId();
+    }
+
+    public static String getSnowflakeNextIdStr() {
+        return IdUtil.getSnowflake(workerId, dataCenterId).nextIdStr();
+    }
+}
--- member/src/main/java/com/criel/train/member/service/MemberService.java
+++ member/src/main/java/com/criel/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.criel.train.member.service;
 import cn.hutool.core.collection.CollUtil;
 import com.criel.train.common.exception.BusinessException;
 import com.criel.train.common.exception.BusinessExceptionEnum;
+import com.criel.train.common.util.SnowUtil;
 import com.criel.train.member.domain.Member;
 import com.criel.train.member.domain.MemberExample;
 import com.criel.train.member.mapper.MemberMapper;
@@ -23,7 +24,7 @@ public class MemberService {
     }
 
     /**
-     * 会员注册功能，暂未完善，供测试
+     * TODO 会员注册功能，暂未完善，供测试
      * @param req 用户注册请求参数
      * @return 用户id
      */
@@ -40,7 +41,7 @@ public class MemberService {
         // 创建新member
         Member member = new Member();
         // 测试用：暂时用系统时间做id
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
