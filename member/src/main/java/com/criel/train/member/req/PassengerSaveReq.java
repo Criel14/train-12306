45a2446980ad7244efaea9865e64840a7b30fc93
Criel14 <1298373079@qq.com>
2025-03-16 16:45:36 +0800

    member：PassengerSaveReq新增字段非空校验

--- member/src/main/java/com/criel/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/criel/train/member/req/PassengerSaveReq.java
@@ -1,6 +1,7 @@
 package com.criel.train.member.req;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,13 +13,16 @@ import java.util.Date;
 public class PassengerSaveReq {
     private Long id;
 
+    @NotNull(message = "会员ID不能为空")
     private Long memberId;
 
     @NotBlank(message = "姓名不能为空")
     private String name;
 
+    @NotBlank(message = "身份证不能为空")
     private String idCard;
 
+    @NotBlank(message = "乘车人类型不能为空")
     private String type;
 
     private Date createTime;
