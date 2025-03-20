74568755fda49180f8638050136c23162c81d4df
Criel14 <1298373079@qq.com>
2025-03-20 23:17:29 +0800

    generator：修改

--- generator/src/main/java/com/criel/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/criel/train/generator/gen/EnumGenerator.java
@@ -2,10 +2,10 @@ package com.criel.train.generator.gen;
 
 
 import cn.hutool.core.util.StrUtil;
-import com.jiawa.train.business.enums.ConfirmOrderStatusEnum;
-import com.jiawa.train.business.enums.SeatColEnum;
-import com.jiawa.train.business.enums.SeatTypeEnum;
-import com.jiawa.train.business.enums.TrainTypeEnum;
+//import com.jiawa.train.business.enums.ConfirmOrderStatusEnum;
+//import com.jiawa.train.business.enums.SeatColEnum;
+//import com.jiawa.train.business.enums.SeatTypeEnum;
+//import com.jiawa.train.business.enums.TrainTypeEnum;
 import com.criel.train.member.enumeration.PassengerTypeEnum;
 
 import java.io.FileOutputStream;
@@ -27,10 +27,10 @@ public class EnumGenerator {
         long begin = System.currentTimeMillis();
         try {
             toJson(PassengerTypeEnum.class, bufferObject, bufferArray);
-            toJson(TrainTypeEnum.class, bufferObject, bufferArray);
-            toJson(SeatTypeEnum.class, bufferObject, bufferArray);
-            toJson(SeatColEnum.class, bufferObject, bufferArray);
-            toJson(ConfirmOrderStatusEnum.class, bufferObject, bufferArray);
+//            toJson(TrainTypeEnum.class, bufferObject, bufferArray);
+//            toJson(SeatTypeEnum.class, bufferObject, bufferArray);
+//            toJson(SeatColEnum.class, bufferObject, bufferArray);
+//            toJson(ConfirmOrderStatusEnum.class, bufferObject, bufferArray);
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
