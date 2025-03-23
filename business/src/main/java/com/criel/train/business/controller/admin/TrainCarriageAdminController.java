950714c19637b0a497ff8733e5d42cd194446cd8
Criel14 <1298373079@qq.com>
2025-03-23 21:58:58 +0800

    business：生成火车车厢相关代码

---
+++ business/src/main/java/com/criel/train/business/controller/admin/TrainCarriageAdminController.java
@@ -0,0 +1,39 @@
+package com.criel.train.business.controller.admin;
+
+import com.criel.train.common.context.LoginMemberContext;
+import com.criel.train.common.resp.CommonResp;
+import com.criel.train.common.resp.PageResp;
+import com.criel.train.business.req.TrainCarriageQueryReq;
+import com.criel.train.business.req.TrainCarriageSaveReq;
+import com.criel.train.business.resp.TrainCarriageQueryResp;
+import com.criel.train.business.service.TrainCarriageService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/train-carriage")
+public class TrainCarriageAdminController {
+
+    @Resource
+    private TrainCarriageService trainCarriageService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody TrainCarriageSaveReq req) {
+        trainCarriageService.save(req);
+        return CommonResp.success();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<TrainCarriageQueryResp>> queryList(@Valid TrainCarriageQueryReq req) {
+        PageResp<TrainCarriageQueryResp> list = trainCarriageService.queryList(req);
+        return CommonResp.success(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        trainCarriageService.delete(id);
+        return CommonResp.success();
+    }
+
+}
