f272f285ed800fdf91f86f3d42b35d0beb6469be
Criel14 <1298373079@qq.com>
2025-03-24 22:34:17 +0800

    business：添加Station的query-all接口

--- business/src/main/java/com/criel/train/business/controller/admin/StationAdminController.java
+++ business/src/main/java/com/criel/train/business/controller/admin/StationAdminController.java
@@ -1,5 +1,6 @@
 package com.criel.train.business.controller.admin;
 
+import com.criel.train.business.resp.TrainQueryResp;
 import com.criel.train.common.resp.CommonResp;
 import com.criel.train.common.resp.PageResp;
 import com.criel.train.business.req.StationQueryReq;
@@ -9,6 +10,8 @@ import com.criel.train.business.service.StationService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/station")
@@ -26,6 +29,12 @@ public class StationAdminController {
     @GetMapping("/query-list")
     public CommonResp<PageResp<StationQueryResp>> queryList(@Valid StationQueryReq req) {
         PageResp<StationQueryResp> list = stationService.queryList(req);
+        return CommonResp.success(list);
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<StationQueryResp>> queryAll() {
+        List<StationQueryResp> list = stationService.queryAll();
         return CommonResp.success(list);
     }
 
