6e3c67d4bdb4e667c54629b8ef9c52fb54ec3ac7
Criel14 <1298373079@qq.com>
2025-04-02 16:21:59 +0800

    测试缓存

--- business/src/main/java/com/criel/train/business/controller/TestController.java
+++ business/src/main/java/com/criel/train/business/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.criel.train.business.controller;
 
+import com.criel.train.business.service.TrainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private TrainService trainService;
+
     @GetMapping("/test-connect")
     public String testConnect() {
         return "business test success";
+    }
+
+    @GetMapping("/test-query")
+    public String testQuery() {
+        trainService.queryAll();
+        return "business testQuery success";
     }
 }
